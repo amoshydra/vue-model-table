848086c7c3bdce119f82d4cd82d6d535472fe380
Amos Wong <amoswwj@gmail.com>
2018-08-15 01:11:05 +0800

    Prepare component for building

---
+++ src/components/vue-table.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    vue-table
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'vue-table',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  margin: 40px 0 0;
+}
+</style>
