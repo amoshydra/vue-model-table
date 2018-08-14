84cd21751408982aef9eebc025e1bc85b36d60b8
Amos Wong <amoswwj@gmail.com>
2018-08-15 01:40:58 +0800

    Prototype table component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
-    <VueTable />
+    <VueTable
+      :fields="fields"
+      :items="items"
+    />
   </div>
 </template>
 
@@ -11,8 +14,44 @@ export default {
   name: 'app',
   components: {
     VueTable
-  }
-}
+  },
+  data() {
+    return {
+      fields: [
+        { key: 'id', label: 'Id' },
+        { key: 'name', label: 'Name' },
+        { key: 'email', label: 'Email' },
+        { key: 'age', label: 'Age' },
+      ],
+      items: [
+        {
+          id: 1,
+          name: 'Amos Wong',
+          email: 'amos@mail.com',
+          age: 25,
+        },
+        {
+          id: 2,
+          name: 'Adam Lao',
+          email: 'adam.lao@email.com',
+          age: 21,
+        },
+        {
+          id: 3,
+          name: 'Stephen Matthew',
+          email: 'sm94@mail.com',
+          age: 24,
+        },
+        {
+          id: 4,
+          name: 'Roger Wiliam',
+          email: 'roger.william@mail.com',
+          age: 54,
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
--- src/components/vue-table.vue
+++ src/components/vue-table.vue
@@ -1,18 +1,48 @@
 <template>
-  <div>
-    vue-table
-  </div>
+  <table class="vue-table">
+    <thead>
+      <tr>
+        <th
+          v-for="field in fields"
+          :key="field.key"
+        >
+          {{ field.label }}
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="(item, index) in items"
+        :key="item.id || index"
+      >
+        <td
+          v-for="field in fields"
+          :key="field.key"
+        >{{ item[field.key] }}</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script>
 export default {
   name: 'vue-table',
+  props: {
+    fields: {
+      type: Array,
+      default: () => [],
+    },
+    items: {
+      type: Array,
+      default: () => [],
+    },
+  },
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1 {
-  margin: 40px 0 0;
+.vue-table {
+  width: 100%;
 }
 </style>
