302dbb8c76fae56fad692059cc0636bf60f47514
Amos Wong <amoswwj@gmail.com>
2018-08-20 05:25:20 +0800

    Implement count function

--- test/mock-server.js
+++ test/mock-server.js
@@ -2,19 +2,27 @@ const example = require('./example.json');
 const MAX_TIMEOUT = 3000;
 const P_BIAS = 32;
 
-const getItem = ({ perPage, offset }) => {
+const mockServerRequest = (handler) => {
   const timeout = Math.max(
     10,
     Math.pow(Math.random(), P_BIAS) * MAX_TIMEOUT
   );
-
   return new Promise((resolve) => {
     setTimeout(() => {
-      resolve(example.slice(offset, perPage));
+      resolve(handler());
     }, timeout);
   });
-};
+}
 
 module.exports = {
-  get: getItem,
+  get: ({ perPage, offset }) => mockServerRequest(
+    () => ({
+      data: example.slice(offset, offset + perPage),
+    })
+  ),
+  count: () => mockServerRequest(
+    () => ({
+      data: example.length,
+    })
+  ),
 };
