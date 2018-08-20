61383ecc23f5c8e7b640e6945ffd5728c4804eb0
Amos Wong <amoswwj@gmail.com>
2018-08-20 08:30:35 +0800

    Add pagination component

---
+++ src/components/vue-table-pagination.vue
@@ -0,0 +1,48 @@
+<template>
+  <nav v-if="vueTable" class="table-pagination">
+    <ul class="pagination">
+      <li
+        class="page-item"
+        :class="{ disabled: vueTable.loading || !vueTable.canMoveBack }"
+      ><a
+        class="page-link"
+        href="#"
+        @click.prevent="vueTable.move(-1)"
+      >Previous</a></li>
+
+      <slot/>
+
+      <li
+        class="page-item"
+        :class="{ disabled: vueTable.loading || !vueTable.canMoveForward }"
+      ><a
+        class="page-link"
+        href="#"
+        @click.prevent="vueTable.move(1)"
+      >Next</a></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: {
+    vueTable: {
+      type: Object,
+      default: () => {},
+    },
+  },
+}
+</script>
+
+<style scoped>
+.table-pagination {
+  display: inline-block;
+}
+.table-pagination__nav {
+  display: inline-block;
+}
+.table-pagination__button {
+  display: inline-block;
+}
+</style>
